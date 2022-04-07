84bffb5f7c1a085b9af20499d7c0b0186605d22c
Kacper Domagala <kacperdomagala99@gmail.com>
2022-04-07 11:03:18 +0200

    Added project template

---
+++ OnlineClassDiaryWebApp/OnlineClassDiaryWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OnlineClassDiaryWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
