d937afbb905a23b8ef04ec30b36c1d711b82a085
Adrian Bruz <adrianbruz@gmail.com>
2022-06-22 14:21:12 +0200

    Back to the previous view

--- online_class_diary_web_app-react/src/pages/LoginPage.jsx
+++ online_class_diary_web_app-react/src/pages/LoginPage.jsx
@@ -63,8 +63,8 @@ const LoginPage = () => {
   return (
     <Wrapper>
       <Form onSubmit={submitHandler}>
-        {/* <h1>E - Diary</h1> */}
-        <img src="assets/logo.png"></img>
+        <h1>E - Diary</h1>
+        {/* <img src="assets/logo.png"></img> */}
         <DataFields>
           <div>
             <input
@@ -134,9 +134,9 @@ const Form = styled.form`
   justify-content: space-around;
   flex-direction: column;
 
-  ${'' /* h1 {
+  h1 {
     color: rgb(233, 186, 125);
-  } */}
+  }
 
   img {
     width: 14vw;
